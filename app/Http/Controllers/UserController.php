c4c5a6939704e282f14796bcccd13dc0b3fc56b7
Shoaib Ahmad <muhammad.taha@asappstudio.com>
2023-08-21 09:57:25 +0500

    work on Api's

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+
+use App\Mail\OtpMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+
+
+
+    // ...
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'phone_number' => 'required|unique:users',
+            'email' => 'required|email|unique:users',
+        ], [
+            'phone_number.unique' => 'Phone number already in use.',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->first(),
+            ], 422); // Unprocessable Entity
+        }
+
+        $otp = $this->generateFourDigitNumber();
+
+        // Create a new user
+        // You'll need to define your User model and its properties
+        $user = new User();
+        $user->username = $request->input('username');
+        $user->phone_number = $request->input('phone_number');
+        $user->email = $request->input('email');
+        $user->otp_code = $otp;
+        $user->save();
+
+        Mail::to($user->email)->send(new OtpMail($otp));
+
+        // Generate a token for the user
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        // Return user information and token in the response
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User registered successfully.',
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+
+
+
+    public function verifyOtp(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'required|string',
+            'otp' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->first(),
+            ], 422); // Unprocessable Entity
+        }
+
+        $user = User::where('phone_number', $request->phone_number)->first();
+
+        if (!$user) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found.'
+            ], 404);
+        }
+
+        // Assuming you have a "otp" field in the users table
+        if ($user->otp_code !== $request->otp) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid OTP.'
+            ], 400);
+        }
+
+        // Clear the OTP field after successful verification
+        $user->otp_code = null;
+        $user->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'OTP verified successfully.'
+        ], 200);
+    }
+
+    public function generateFourDigitNumber()
+    {
+        return rand(1000, 9999);
+    }
+    public function resendOtp(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->first(),
+            ], 422); // Unprocessable Entity
+        }
+        $user = User::where('phone_number', $request->phone_number)->first();
+
+        if (!$user) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Phone number not found.'
+            ], 404);
+        }
+
+        // Generate a new OTP and save it to the user's record
+        $newOtp = $this->generateFourDigitNumber(); // Implement this function
+        $user->otp_code = $newOtp;
+        $user->save();
+
+        // Send the new OTP to the user (you can use your email sending logic here)
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'New OTP sent successfully.'
+        ], 200);
+    }
+
+
+    public function createPassword(Request $request)
+    {
+
+
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'required|string',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->first(),
+            ], 422); // Unprocessable Entity
+        }
+
+        $user = User::where('phone_number', $request->phone_number)->first();
+
+        if (!$user) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found.'
+            ], 404);
+        }
+
+        // Set the user's password
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Password created successfully.'
+        ], 200);
+    }
+
+
+
+    // ...
+
+    public function signIn(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+       
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Sign-in successful.',
+                'token' => $token,
+            ], 200);
+        }
+
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Invalid username or password.'
+        ], 401);
+    }
+}
