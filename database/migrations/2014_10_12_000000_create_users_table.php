7cbff0cec958193f9f1619059e821c2f77313b25
obaid <obaidwazeer@gmail.com>
2023-09-12 03:07:43 -0700

    migration files added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('phone_number')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->enum('user_type',['admin','user']);
             $table->rememberToken();
             $table->timestamps();
         });
